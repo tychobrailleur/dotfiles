141778266a2ff694882bf522c3a00fa975ee86b1
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-02-28 06:09:11 +0000

    Update leiningen profile.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[refactor-nrepl "2.4.0"]
-                  [cider/cider-nrepl "0.22.0"]]}}
+                  [cider/cider-nrepl "0.24.0"]
+                  [lein-license "0.1.8"]]}}
