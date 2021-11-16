46f7add027ecef3399a686ed93a07cc35bffcac4
Sébastien Le Callonnec <sebastien@weblogism.com>
2021-11-16 17:28:12 +0000

    Add alembic to reload deps without restarting REPL.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,8 @@
-{:user {:plugins [[refactor-nrepl "2.5.0"]
+{:user {:dependencies [[alembic "0.3.2"]]
+        :plugins [[refactor-nrepl "3.1.0"]
                   [cider/cider-nrepl "0.26.0"]
-                  [lein-license "1.0.0"]]}}
+                  [lein-license "1.0.0"]
+                  [com.gfredericks/lein-shorthand "0.4.1"]]
+        :shorthand {. [clojure.pprint/pprint
+                       alembic.still/distill
+                       alembic.still/lein]}}}
