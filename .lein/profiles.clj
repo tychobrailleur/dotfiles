a7dddf2f925b9349df4668c09e39ce8f9836fc8c
Sébastien Le Callonnec <sebastien@weblogism.com>
2022-01-07 17:39:43 +0000

    Remove shorthand, update cider-nrepl, and add count.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,5 @@
 {:user {:dependencies [[alembic "0.3.2"]]
         :plugins [[refactor-nrepl "3.1.0"]
-                  [cider/cider-nrepl "0.26.0"]
+                  [cider/cider-nrepl "0.27.3"]
                   [lein-license "1.0.0"]
-                  [com.gfredericks/lein-shorthand "0.4.1"]]
-        :shorthand {. [clojure.pprint/pprint
-                       alembic.still/distill
-                       alembic.still/lein]}}}
+                  [lein-count "1.0.9"]]}}
